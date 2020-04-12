c9bbdca326a22952876d7030f0ab62802c0a1d90
Ryan Doherty <rjd.atx@gmail.com>
2020-04-11 19:46:49 -0500

    React Router anchor tags / links to Link router methodology

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 const PageOne = () =>
 {
-    return <div>PageOne</div>
+    return (
+        <div>
+            PageOne
+            <Link to="/pagetwo">Navigate to Page Two</Link>
+        </div>
+    );
 };
 
 const PageTwo = () =>
@@ -12,6 +17,7 @@ const PageTwo = () =>
         <div>
             PageTwo
             <button>Click Me!</button>
+            <Link to="/">Navigate to Page One</Link>
         </div>
     );
 };
