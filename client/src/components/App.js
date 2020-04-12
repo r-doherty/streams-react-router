b43ed044bd3ac26fbace4c260d7aa9e409b6101d
Ryan Doherty <rjd.atx@gmail.com>
2020-04-11 20:13:44 -0500

    Scaffolding Components (streams x 5)

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,26 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
-
-const PageOne = () =>
-{
-    return (
-        <div>
-            PageOne
-            <Link to="/pagetwo">Navigate to Page Two</Link>
-        </div>
-    );
-};
-
-const PageTwo = () =>
-{
-    return (
-        <div>
-            PageTwo
-            <button>Click Me!</button>
-            <Link to="/">Navigate to Page One</Link>
-        </div>
-    );
-};
+import { Route, Link, BrowserRouter } from 'react-router-dom';
 
 const App = () =>
 {
@@ -28,8 +7,11 @@ const App = () =>
         <div>
             <BrowserRouter>
                 <div>
-                    <Route path="/" exact component={PageOne}/>
-                    <Route path="/pageTwo" component={PageTwo}/>
+                    <Route path="/" exact component={StreamList}/>
+                    <Route path="/streams/net" component={StreamCreate}/>
+                    <Route path="/streams/edit" component={StreamEdit}/>
+                    <Route path="/streams/delete" component={StreamDelete}/>
+                    <Route path="/streams/show" component={StreamShow}/>
                 </div>
             </BrowserRouter>
         </div>
