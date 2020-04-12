a682e5ecd032b82483bfede7e3f473cfe9200858
Ryan Doherty <rjd.atx@gmail.com>
2020-04-11 20:30:39 -0500

    connecting the header -- links inside routers

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import { Route, Link, BrowserRouter } from 'react-router-dom';
+import { Route, BrowserRouter } from 'react-router-dom';
 import StreamCreate from './streams/StreamCreate';
 import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
+import Header from './Header';
 
 const App = () =>
 {
     return (
-        <div>
-            <h1>Header!</h1>
+        <div className="ui container">
             <BrowserRouter>
                 <div>
+                    <Header />
                     <Route path="/" exact component={StreamList}/>
                     <Route path="/streams/new" component={StreamCreate}/>
                     <Route path="/streams/edit" component={StreamEdit}/>
