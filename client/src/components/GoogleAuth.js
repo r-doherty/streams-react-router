e2adacc37f87088792b08ca22bca90ccb1f3f8bb
Ryan Doherty <rjd.atx@gmail.com>
2020-04-12 12:40:17 -0500

    On-Demand Sign in and Sign Out -- button functionality

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -20,6 +20,14 @@ class GoogleAuth extends React.Component {
         this.setState({ isSignedIn: this.auth.isSignedIn.get() });
     };
 
+    onSignIn = () => {
+        this.auth.signIn();
+    };
+
+    onSignOut = () => {
+        this.auth.signOut();
+    };
+
     renderAuthButton() {
         if (this.state.isSignedIn === null)
         {
@@ -29,7 +37,7 @@ class GoogleAuth extends React.Component {
         else if (this.state.isSignedIn)
         {
             return (
-                <button className="ui red google button">
+                <button onClick={this.onSignOut} className="ui red google button">
                     <i className="google icon" />
                     Sign Out
                 </button>
@@ -38,7 +46,7 @@ class GoogleAuth extends React.Component {
         else
         {
             return (
-                <button className="ui green google button">
+                <button onClick={this.onSignIn} className="ui green google button">
                     <i className="google icon" />
                     Sign In with Google
                 </button>
