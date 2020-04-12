7ccb5da138791d3b8e6f354ff084ababf44515b7
Ryan Doherty <rjd.atx@gmail.com>
2020-04-12 16:14:17 -0500

    Validation of Form Inputs

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -26,6 +26,21 @@ class StreamCreate extends React.Component {
     }
 }
 
+const validate = (formValues) => {
+    const errors = {};
+    
+    if (!formValues.title) {
+        // will only run if the user did not enter a title
+        errors.title = 'You must enter a title';
+    }
+
+    if(!formValues.description) {
+        errors.description = 'You must enter a description';
+    }
+
+    return errors;
+};
+
 export default reduxForm({
     form: 'streamCreate'
 })(StreamCreate);
