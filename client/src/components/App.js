a89b694bfb6c473a6d344ea38bbcfdeafe959cbf
Ryan Doherty <rjd.atx@gmail.com>
2020-04-14 15:29:10 -0500

    delete a stream process

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import StreamCreate from './streams/StreamCreate';
 import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
@@ -8,18 +8,19 @@ import StreamShow from './streams/StreamShow';
 import Header from './Header';
 import history from '../history';
 
-const App = () =>
-{
+const App = () => {
     return (
         <div className="ui container">
             <Router history={history}>
                 <div>
                     <Header />
-                    <Route path="/" exact component={StreamList}/>
-                    <Route path="/streams/new" component={StreamCreate}/>
-                    <Route path="/streams/edit/:id" component={StreamEdit}/>
-                    <Route path="/streams/delete" component={StreamDelete}/>
-                    <Route path="/streams/show" component={StreamShow}/>
+                    <Switch>
+                        <Route path="/" exact component={StreamList}/>
+                        <Route path="/streams/new" exact component={StreamCreate}/>
+                        <Route path="/streams/edit/:id" exact component={StreamEdit}/>
+                        <Route path="/streams/delete/:id" exact component={StreamDelete}/>
+                        <Route path="/streams/show" exact component={StreamShow}/>
+                    </Switch>
                 </div>
             </Router>
         </div>
